4c7353806830f30639e3ceb3f68e8797c57009e1
Tom Ritchford <tom@swirly.com>
2023-02-25 18:46:21 +0100

    Move code from rec/rec

---
+++ content/on-programming.md
@@ -0,0 +1,8 @@
+Title: On computer programming
+Category: General
+
+This is a placeholder.
+
+    # This is Python code
+    for i in range(3):
+        print(i)
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+AUTHOR = 'Tom Ritchford'
+SITENAME = 'On computer programming'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Amsterdam'
+
+DEFAULT_LANG = 'en'
+DEFAULT_DATE = 'fs'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Python.org', 'https://www.python.org/'),
+)
+
+# Social widget
+SOCIAL = (
+    ('github', 'https://github.com/rec'),
+    ('Mastodon', 'https://toot.community/@TomSwirly'),
+)
+
+DEFAULT_PAGINATION = 12
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
